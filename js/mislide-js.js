7761d64b51511a60b88668f090349dbe47f12ec4
irenefb <35541608+irenefb@users.noreply.github.com>
2020-05-19 13:02:07 +0200

    Ordenar código

--- js/mislide-js.js
+++ js/mislide-js.js
@@ -30,7 +30,6 @@ function inicia(){
 		izq.style.zIndex= slidesN+1;
 	})
 
-	
 	//ver el siguiente slide
 	dch.appendChild(caja);
 	caja.setAttribute("class","slideSiguiente");
@@ -40,7 +39,6 @@ function inicia(){
 			caja.style.width = (w/2-mrgIzq-2) + "px";
 		}
 	
-
 	//ver el slide anterior
 	izq.appendChild(cajaI);
 	cajaI.setAttribute("class","slideAnterior");
@@ -50,37 +48,28 @@ function inicia(){
 			cajaI.style.width = (w/2-mrgDch-2) + "px";
 		}	
 }
-
 inicia();
-
 
 
 //flecha derecha
 function avanza(){
-
 	slidesA[slidesN-1].classList.add("oculta");
-
-
-		//slider siguiente
-		var li = document.querySelector(".slideSiguiente li");
-		li.classList.add("saleIzq");
-		//slider anterior
-		var liI = document.querySelector(".slideAnterior li");
-		liI.classList.add("saleIzqI");
+	//slider siguiente
+	var li = document.querySelector(".slideSiguiente li");
+	li.classList.add("saleIzq");
+	//slider anterior
+	var liI = document.querySelector(".slideAnterior li");
+	liI.classList.add("saleIzqI");
 
 	setTimeout(function(){
-
 		var duplicado = slidesA.slice((slidesN-1),(slidesN))
 		slidesA.pop();
 		slidesA.unshift(duplicado[0]);
 		
 		slidesA.forEach(function(valor,item,array) {
-
 			slidesA[item].style.zIndex = item+1;
 			slidesA[item].classList.remove("oculta");
-
 		})
-		
 	
 		//slider siguiente
 		document.querySelector(".slideSiguiente").removeChild(li);
@@ -89,24 +78,18 @@ function avanza(){
 		caja.appendChild(contenido);
 		contenido.setAttribute("class","entraIzq");
 
-
 		//slider anterior
 		document.querySelector(".slideAnterior").removeChild(liI);
 		var contenidoIntermedioI = slidesA.slice(0,1);
 		var contenidoI = contenidoIntermedioI[0].cloneNode(true);
 		cajaI.appendChild(contenidoI);
 		contenidoI.setAttribute("class","entraIzqI");
-
-
 	},1000)
-
 }
-
 dch.addEventListener("click", avanza);
 
 //flecha izquierda
 function retrocede(){
-
 	var duplicado = slidesA.slice(0,1)//duplico el item0
 	slidesA.shift();//elimino el item0
 	slidesA.push(duplicado[0])
@@ -118,18 +101,13 @@ function retrocede(){
 	var liI = document.querySelector(".slideAnterior li");
 	liI.classList.add("saleIzqI");
 
-
-
 	slidesA.forEach(function(valor,item,array) {
-
 		slidesA[item].style.zIndex = item+1;
-
 	})
 
 	slidesA[slidesN-1].classList.add("muestra");
 
 	setTimeout(function(){
-
 		slidesA[slidesN-1].classList.remove("muestra");
 
 		//slider siguiente
@@ -144,18 +122,9 @@ function retrocede(){
 		var contenidoI = contenidoIntermedioI[0].cloneNode(true);
 		cajaI.appendChild(contenidoI);
 		contenidoI.setAttribute("class","entraIzqI");
-			
-			
-	
 	},1000);
-
-
-
 }
-
 izq.addEventListener("click", retrocede)
-
-
 
 //mostrar próximo slide
 function mostrarAvanza(){
@@ -170,18 +139,15 @@ if(w > 1024){
 
 }
 
-
-
 function ocultarAvanza(){
 	caja.classList.remove("mostrar");
 	document.querySelector(".slideSiguiente").style.right =  "0px";
 }
-
 if(w > 1024){
 	dch.addEventListener("mouseleave",ocultarAvanza);
 }
 
-	//mostrar el slide anterior
+//mostrar el slide anterior
 function mostrarAtras(){
 	cajaI.classList.add("mostrar");
 	document.querySelector(".slideAnterior").style.left = mrgIzq + "px";
@@ -200,14 +166,12 @@ if(w > 1024){
 	izq.addEventListener("mouseleave",ocultarAtras);
 }
 
-
 //cambio de tamaño de pantalla
 window.onresize = function(){
 	var w = window.innerWidth;
 	if(w > 1024){
-		console.log("holi")
-		ocultarAvanza()
-		ocultarAtras()
+		ocultarAvanza();
+		ocultarAtras();
 	}else{
 		mostrarAtras();
 		mostrarAvanza();
@@ -216,5 +180,4 @@ window.onresize = function(){
 		caja.style.width = (w/2-mrgIzq-2) + "px";
 		cajaI.style.width = (w/2-mrgDch-2) + "px";
 	}
-
 }
